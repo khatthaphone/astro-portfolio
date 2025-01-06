fcd274c7e00f1a98d0ad08e38c620cf4297b68d9
Khatthaphone <8247636+khatthaphone@users.noreply.github.com>
2025-01-06 18:29:44 +0700

    Fix toggle dark mode

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,13 +1,9 @@
-import * as React from 'react'
 import { Moon, Sun } from 'lucide-react'
+import * as React from 'react'
 
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+
+const themes = ['light', 'dark', 'system']
 
 export function ModeToggle() {
   const [theme, setThemeState] = React.useState<'light' | 'dark' | 'system'>(
@@ -32,26 +28,42 @@ export function ModeToggle() {
     localStorage.setItem('theme', theme)
   }
 
+  const onNextTheme = () => {
+
+    const currentIndex = themes.indexOf(theme)
+    if (currentIndex === -1) {
+      return themes[0]
+    }
+
+    // Get the next theme, cycling back to the first if at the end
+    setThemeState(themes[(currentIndex + 1) % themes.length] as 'light' | 'dark' | 'system')
+  }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => onChangeTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => onChangeTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => onChangeTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="outline" size="icon" onClick={() => onNextTheme()}>
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+    // <DropdownMenu>
+    //   <DropdownMenuTrigger asChild>
+    //     <Button variant="outline" size="icon">
+    //       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    //       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    //       <span className="sr-only">Toggle theme</span>
+    //     </Button>
+    //   </DropdownMenuTrigger>
+    //   <DropdownMenuContent align="end">
+    //     <DropdownMenuItem onClick={() => onChangeTheme('light')}>
+    //       Light
+    //     </DropdownMenuItem>
+    //     <DropdownMenuItem onClick={() => onChangeTheme('dark')}>
+    //       Dark
+    //     </DropdownMenuItem>
+    //     <DropdownMenuItem onClick={() => onChangeTheme('system')}>
+    //       System
+    //     </DropdownMenuItem>
+    //   </DropdownMenuContent>
+    // </DropdownMenu>
   )
 }
